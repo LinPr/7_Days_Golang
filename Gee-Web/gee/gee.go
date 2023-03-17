4b43f60463f4e6ea910c02ba230255b278ca0af4
LinPr <314573849@qq.com>
2023-03-17 02:48:12 -0700

    add route group control

--- Gee-Web/gee/gee.go
+++ Gee-Web/gee/gee.go
@@ -7,8 +7,22 @@ import (
 
 type HandlerFunc func(*Context)
 
+// Engine 作为调度全局的对象
 type Engine struct {
-	router *router
+	router       *router
+	*RouterGroup                // 继承RouterGroup的方法
+	groups       []*RouterGroup // stroe all groups
+}
+
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc // support middleware
+	parent      *RouterGroup  // support nesting
+	engine      *Engine       // all groups share a Engine interface
+}
+
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
 }
 
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
@@ -16,23 +30,49 @@ func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	engine.router.handle(c)
 }
 
-func New() *Engine {
-	return &Engine{router: newRouter()}
+/*
+	The NewEngine function
+
+1. creates a new instance of the Engine struct, initializes the router,
+2. and sets up the RouterGroup as the root group of the engine.
+3. It then adds the root group to the list of groups managed by the engine.
+*/
+func NewEngine() *Engine {
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = append(engine.groups, engine.RouterGroup)
+
+	return engine
 }
 
-func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+/*
+	The NewGroup method of the RouterGroup struct
+
+1. creates a new instance of the RouterGroup struct, which inherits the middleware stack and prefix of its parent group.
+2. The new group is added to the list of groups managed by the engine.
+*/
+func (group *RouterGroup) NewGroup(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		// 做一个继承
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *RouterGroup) addRoute(method string, component string, handler HandlerFunc) {
+	pattern := group.prefix + component
 	log.Printf("Route %4s-%s", method, pattern)
-	engine.router.addRoute(method, pattern, handler)
+	group.engine.router.addRoute(method, pattern, handler)
 }
 
-func (engine *Engine) GET(pattern string, handler HandlerFunc) {
-	engine.addRoute("GET", pattern, handler)
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
 }
 
-func (engine *Engine) POST(pattern string, handler HandlerFunc) {
-	engine.addRoute("POST", pattern, handler)
-}
-
-func (engine *Engine) Run(addr string) (err error) {
-	return http.ListenAndServe(addr, engine)
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
 }
--- Gee-Web/main.go
+++ Gee-Web/main.go
@@ -1,29 +1,37 @@
 package main
 
 /*
-(1)
-$ curl -i http://localhost:9999/
+(1) index
+curl -i http://localhost:9999/index
 HTTP/1.1 200 OK
-Date: Mon, 12 Aug 2019 16:52:52 GMT
+Date: Sun, 01 Sep 2019 08:12:23 GMT
+Content-Length: 19
+Content-Type: text/html; charset=utf-8
+<h1>Index Page</h1>
+
+(2) v1
+$ curl -i http://localhost:9999/v1/
+HTTP/1.1 200 OK
+Date: Mon, 12 Aug 2019 18:11:07 GMT
 Content-Length: 18
 Content-Type: text/html; charset=utf-8
 <h1>Hello Gee</h1>
 
-(2)
-$ curl "http://localhost:9999/hello?name=geektutu"
-hello geektutu, you're at /hello
-
 (3)
-$ curl "http://localhost:9999/hello/geektutu"
-hello geektutu, you're at /hello/geektutu
+$ curl "http://localhost:9999/v1/hello?name=geektutu"
+hello geektutu, you're at /v1/hello
 
 (4)
-$ curl "http://localhost:9999/assets/css/geektutu.css"
-{"filepath":"css/geektutu.css"}
+$ curl "http://localhost:9999/v2/hello/geektutu"
+hello geektutu, you're at /hello/geektutu
 
 (5)
-$ curl "http://localhost:9999/xxx"
-404 NOT FOUND: /xxx
+$ curl "http://localhost:9999/v2/login" -X POST -d 'username=geektutu&password=1234'
+{"password":"1234","username":"geektutu"}
+
+(6)
+$ curl "http://localhost:9999/hello"
+404 NOT FOUND: /hello
 */
 
 import (
@@ -33,28 +41,39 @@ import (
 )
 
 func main() {
-	r := gee.New()
-	r.GET("/", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	e := gee.NewEngine()
+	e.GET("/index", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
-	r.GET("/hello", func(c *gee.Context) {
-		c.String(http.StatusOK, "hello %s, you are at %s\n", c.Query("name"), c.Path)
-	})
-	r.GET("/data", func(c *gee.Context) {
-		c.Data(http.StatusOK, []byte("hello %s, you are at %s\n"))
-	})
-	r.POST("/login", func(c *gee.Context) {
-		c.JSON(http.StatusOK, gee.H{
-			"username: ": c.PostForm("username"),
-			"password: ": c.PostForm("password"),
+	v1 := e.NewGroup("/v1")
+	{
+		v1.GET("/", func(c *gee.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello gee</h1>")
 		})
-	})
-	r.GET("/hello/:name/geektutu", func(c *gee.Context) {
-		// expect /hello/something/geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
-	r.GET("/assets/*filepath", func(c *gee.Context) {
-		c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
-	})
-	r.Run(":9999")
+		v1.GET("/hello", func(c *gee.Context) {
+			c.String(http.StatusOK, "hello %s, you are at %s\n", c.Query("name"), c.Path)
+		})
+		v1_1 := v1.NewGroup("/v1_1")
+		{
+			v1_1.GET("/world", func(c *gee.Context) {
+				c.HTML(http.StatusOK, "<h1>Hello this is world</h1>")
+			})
+		}
+	}
+
+	v2 := e.NewGroup("/v2")
+	{
+		v2.GET("/hello/:name/geektutu", func(c *gee.Context) {
+			// expect /hello/something/geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *gee.Context) {
+			c.JSON(http.StatusOK, gee.H{
+				"username: ": c.PostForm("username"),
+				"password: ": c.PostForm("password"),
+			})
+		})
+	}
+
+	e.Run(":9999")
 }
